eb0ef86fa43f0961fec4e732e3c87ea5dea17da7
Bikna <hbikna@gmail.com>
2025-01-23 14:32:54 -0800

    finished tabs in workshop

--- Unit-3/unit3.puppypals/src/App.jsx
+++ Unit-3/unit3.puppypals/src/App.jsx
@@ -1,17 +1,45 @@
 import React, { useState } from "react";
 import { puppyList } from "./data.js";
+import "./App.css";
 
 function App() {
-  console.log(puppyList);
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
+  function handleClick() {
+    // some logic here
+  }
 
   return (
-    <>
-      <h1>Puppy Pals</h1>{" "}
-      {puppies.map((puppy) => (
-        <p>{puppy.name}</p>
-      ))}
-    </>
+    <div className="App">
+      <h1>Puppy Pals</h1>
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              console.log("puppy id: ", puppy.id); // Step 1: Log the puppy's ID to the console
+              setFeatPupId(puppy.id); // Step 2: Update the state with the clicked puppy's ID
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+
+      {/* Step 3: Conditionally render the featured puppy ID */}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
 
