a119a4acf2c1e4a165d2adeac551dea3dbc48b59
Bikna <hbikna@gmail.com>
2025-01-23 12:17:51 -0800

    first commit

---
+++ Unit-3/unit3.puppypals/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { puppyList } from "./data.js";
+
+function App() {
+  console.log(puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
+
+  return (
+    <>
+      <h1>Puppy Pals</h1>{" "}
+      {puppies.map((puppy) => (
+        <p>{puppy.name}</p>
+      ))}
+    </>
+  );
+}
+
+export default App;
